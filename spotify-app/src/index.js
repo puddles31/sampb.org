88ef99a758d62206d2e622d8cc4354f76ef61890
Sam Borg <samuelborg31@gmail.com>
2023-02-25 19:06:51 +0000

    progress on spotify app

--- spotify-app/src/index.js
+++ spotify-app/src/index.js
@@ -1,12 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react';
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import SpotifyNowPlaying from './spotify/SpotifyNowPlaying';
 
-// const spotifyContainer = document.getElementById("spotify-container");
-// const root = ReactDOM.createRoot(root);
+
 const container = document.getElementById('root');
-const root = createRoot(container);
+const root = ReactDOM.createRoot(container);
 
 root.render(
     <React.StrictMode>
@@ -15,3 +14,31 @@ root.render(
     </ChakraProvider>
   </React.StrictMode>
 );
+
+
+// 'use strict';
+
+// const e = React.createElement;
+
+// class LikeButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { liked: false };
+//   }
+
+//   render() {
+//     if (this.state.liked) {
+//       return 'You liked this.';
+//     }
+
+//     return e(
+//       'button',
+//       { onClick: () => this.setState({ liked: true }) },
+//       'Like'
+//     );
+//   }
+// }
+
+// const domContainer = document.querySelector('#spotify_container');
+// const root = ReactDOM.createRoot(domContainer);
+// root.render(e(LikeButton));
